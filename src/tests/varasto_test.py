136943c6dd48d427e2f627817747ded59f0911d2
eerojantunen <jant.eero@gmail.com>
2024-11-04 23:02:49 +0200

    rikkoo testit

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -5,7 +5,7 @@ from varasto import Varasto
 class TestVarasto(unittest.TestCase):
     def setUp(self):
         self.varasto = Varasto(10)
-
+def
     def test_konstruktori_luo_tyhjan_varaston(self):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
