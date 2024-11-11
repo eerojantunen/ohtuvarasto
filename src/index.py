1a8014b96b2e819a50b0ecee2c74f8e16bbdaea4
eerojantunen <jant.eero@gmail.com>
2024-11-11 19:38:24 +0200

    t2+3 alku

--- src/index.py
+++ src/index.py
@@ -1,19 +1,22 @@
 from varasto import Varasto
 
 
-def main():
+
+def alusta():
     mehua = Varasto(100.0)
     olutta = Varasto(100.0, 20.2)
-
     print("Luonnin jälkeen:")
     print(f"Mehuvarasto: {mehua}")
     print(f"Olutvarasto: {olutta}")
+    return mehua,olutta
 
+def temp2(a):
     print("Olut getterit:")
-    print(f"saldo = {olutta.saldo}")
-    print(f"tilavuus = {olutta.tilavuus}")
-    print(f"paljonko_mahtuu = {olutta.paljonko_mahtuu()}")
+    print(f"saldo = {a}")
+    print(f"tilavuus = {a}")
+    print(f"paljonko_mahtuu = {a.paljonko_mahtuu()}")
 
+def mehuxd(mehua):
     print("Mehu setterit:")
     print("Lisätään 50.7")
     mehua.lisaa_varastoon(50.7)
@@ -22,37 +25,39 @@ def main():
     mehua.ota_varastosta(3.14)
     print(f"Mehuvarasto: {mehua}")
 
+def virhetilanne():
     print("Virhetilanteita:")
     print("Varasto(-100.0);")
     huono = Varasto(-100.0)
     print(huono)
-
     print("Varasto(100.0, -50.7)")
     huono = Varasto(100.0, -50.7)
     print(huono)
 
+def virhetilanne2(olutta,mehua):
     print(f"Olutvarasto: {olutta}")
     print("olutta.lisaa_varastoon(1000.0)")
     olutta.lisaa_varastoon(1000.0)
     print(f"Olutvarasto: {olutta}")
-
     print(f"Mehuvarasto: {mehua}")
     print("mehua.lisaa_varastoon(-666.0)")
     mehua.lisaa_varastoon(-666.0)
     print(f"Mehuvarasto: {mehua}")
-
     print(f"Olutvarasto: {olutta}")
+
+def main():
+    mehua,olutta=alusta()
+    temp2(olutta)
+    virhetilanne()
+    virhetilanne2(olutta,mehua)
     print("olutta.ota_varastosta(1000.0)")
     saatiin = olutta.ota_varastosta(1000.0)
     print(f"saatiin {saatiin}")
     print(f"Olutvarasto: {olutta}")
-
     print(f"Mehuvarasto: {mehua}")
     print("mehua.otaVarastosta(-32.9)")
     saatiin = mehua.ota_varastosta(-32.9)
     print(f"saatiin {saatiin}")
     print(f"Mehuvarasto: {mehua}")
-
-
 if __name__ == "__main__":
     main()
